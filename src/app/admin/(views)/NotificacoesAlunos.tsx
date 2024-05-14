1af158fdc77279def45adc75ea0cdb6024023279
Adriano Silva <adrsilva613@gmail.com>
2024-05-14 00:16:17 -0300

    #

--- src/app/admin/(views)/NotificacoesAlunos.tsx
+++ src/app/admin/(views)/NotificacoesAlunos.tsx
@@ -1,14 +1,5 @@
 import { TabelaNotificacaoAlunosTurma } from '../components/tables/NotificacoesTurmas/tabela-alunos-turma'
 
 export default function Notificacoes() {
-  return (
-    <TabelaNotificacaoAlunosTurma
-      data={[
-        {
-          id: crypto.randomUUID(),
-          nome: 'Teste da silva',
-        },
-      ]}
-    />
-  )
+  return <TabelaNotificacaoAlunosTurma data={[]} />
 }
