7aac2398d0b6837c95bf6609617242f5e30cf0b4
Adriano Silva <adrsilva613@gmail.com>
2024-05-26 15:50:01 -0300

    #

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has('session-user')) {
+  if (
+    !request.cookies.has('session-user') ||
+    !request.cookies.has('session-company')
+  ) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 }
