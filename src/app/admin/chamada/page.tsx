51b603d8251d8d27e0932a97ca6275c0bf818830
Adriano Silva <adrsilva613@gmail.com>
2024-10-08 19:44:34 -0300

    [Chamada] Adição de porcentagem e opção de todos alunos presentes

--- src/app/admin/chamada/page.tsx
+++ src/app/admin/chamada/page.tsx
@@ -1,15 +1,95 @@
-import { Loader2 } from 'lucide-react'
-import dynamic from 'next/dynamic'
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Separator } from '@/components/ui/separator'
+import { Skeleton } from '@/components/ui/skeleton'
+
+import { buscarAlunosTurma, buscarTurmas } from '../api/turma'
+import { FormChamadaAlunos } from '../components/forms/Turma/FormChamadaTurma'
 
 export default function PageChamada() {
-  const ChamadaAlunos = dynamic(() => import('../(views)/ChamadaAlunos'), {
-    loading: () => (
-      <div className="flex justify-center mt-4">
-        <Loader2 className="animate-spin" />
-      </div>
-    ),
-    ssr: false,
+  const [openFilterTurma, setOpenFilterTurma] = useState(false)
+  const [turmaSelecionada, setTurma] = useState<string>('')
+
+  const { data: listaTurmas, isLoading: carregandoTurmas } = useQuery({
+    queryKey: ['turmasEscolaChamada'],
+    queryFn: buscarTurmas,
+    staleTime: Infinity,
   })
 
-  return <ChamadaAlunos />
+  const { data: alunosTurma, isLoading: carregandoAlunos } = useQuery({
+    queryKey: ['listaAlunosTurmaNotificacao', turmaSelecionada],
+    queryFn: () => buscarAlunosTurma(turmaSelecionada),
+    enabled: !!turmaSelecionada,
+    staleTime: 0,
+  })
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Chamada diária</CardTitle>
+        <CardDescription>
+          Área para realizar chamada do dia de cada turma
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <section className="grid space-y-4">
+          {carregandoTurmas ? (
+            <Skeleton className="h-4 w-full rounded" />
+          ) : (
+            <Select
+              onOpenChange={setOpenFilterTurma}
+              onValueChange={setTurma}
+              value={turmaSelecionada}
+              open={openFilterTurma}
+              disabled={carregandoTurmas}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione uma turma" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Turmas</SelectLabel>
+                  {listaTurmas?.map((turma) => {
+                    return (
+                      <SelectItem key={turma.id} value={turma.id}>
+                        {turma.nome}
+                      </SelectItem>
+                    )
+                  })}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+          {alunosTurma && alunosTurma.length > 0 && (
+            <>
+              <Separator />
+              <FormChamadaAlunos
+                listaAlunosTurma={alunosTurma}
+                carregandoAlunos={carregandoAlunos}
+              />
+            </>
+          )}
+        </section>
+      </CardContent>
+    </Card>
+  )
 }
