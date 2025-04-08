6086f6fda922cd7e51502ce0a3c105905534ee4e
Adriano Silva <adrsilva613@gmail.com>
2025-04-08 17:34:13 -0300

    refatora: move componente BotaoVoltar para novo arquivo e ajusta importação

--- src/app/admin/turmas/aluno/[id]/page.tsx
+++ src/app/admin/turmas/aluno/[id]/page.tsx
@@ -9,7 +9,6 @@ import { consultaDadosMatricula } from '@/app/admin/api/matricula'
 import { EdicaoMatriculaAlunoDialog } from '@/app/admin/components/dialogs/edicao-matricula-aluno'
 import { AdicionaResponsavelAlunoDialog } from '@/app/admin/components/dialogs/novo-responsavel'
 import { TabelaReponsaveisAluno } from '@/app/admin/components/tables/Responsaveis/tabela-responsavel'
-import { BotaoVoltar } from '@/components/header/BotaoVoltar'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import {
@@ -27,6 +26,7 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 import { aplicarMascaraDocumento, mascararNome } from '@/lib/utils'
+import { BotaoVoltar } from '@/components/header/app-headerbar-back'
 
 export default function MatriculaAlunoPage() {
   const params = useParams<{ id: string }>()
