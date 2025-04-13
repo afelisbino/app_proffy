d9ef3da9cb90134122b7c4c3276a63a889e7a68f
Adriano Silva <adrsilva613@gmail.com>
2025-04-13 17:14:44 -0300

    refactor: corrigir a remoção de cookies na função encerrarSessao

--- src/api/session.ts
+++ src/api/session.ts
@@ -7,8 +7,8 @@ import { redirect } from 'next/navigation'
 export async function encerrarSessao() {
   const cookieStore = cookies()
 
-  cookieStore.delete('session-user')
-  cookieStore.delete('session-company')
+  cookieStore.set('session-user','', { maxAge: 0 })
+  cookieStore.set('session-company','', { maxAge: 0 })
 
   redirect("/login")
 }
