4c3b33be75b4e92039b354c07f8ca929f8257289
Adriano Silva <adrsilva613@gmail.com>
2025-04-13 15:28:38 -0300

    refactor: otimizar a manipulação de cookies na função encerrarSessao e ajustar a correspondência de rotas no middleware

--- src/api/session.ts
+++ src/api/session.ts
@@ -1,13 +1,15 @@
 'use server'
 
 import { cookies } from 'next/headers'
-
 import { redirect } from 'next/navigation'
 
-export async function encerrarSessao() {
-  cookies().delete('session-user')
-  cookies().delete('session-company')
 
-  redirect('/login')
+export async function encerrarSessao() {
+  const cookieStore = cookies()
+
+  cookieStore.delete('session-user')
+  cookieStore.delete('session-company')
+
+  redirect("/login")
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -21,6 +21,6 @@ export const config = {
       * - _next/image (image optimization files)
       * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|login|admin|classe).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|login|admin|classe|*.png|*.jpg).*)',
   ],
 }
